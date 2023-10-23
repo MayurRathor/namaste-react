a059a4bdab9e0ec32611bd1c878d93509f73a4a5
Mayur Rathor <69413513+MayurRathor@users.noreply.github.com>
2023-10-23 12:31:00 +0530

    Episode 1

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+
+
+const heading = React.createElement("div",
+{id:"heading",xyz:"abc"},
+[React.createElement("h1",
+{id:"children",xyz:"abc"},
+"hi Mayur form React"),React.createElement("h1",
+{id:"children",xyz:"abc"},
+"hi Mayur form React"),React.createElement("h1",
+{id:"children",xyz:"abc"},
+"hi Mayur form React")]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading)
